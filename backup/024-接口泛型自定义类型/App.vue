d6b8b58bd86be31f14db259a7dbf4d486fb166ea
Victor.Li <victor.li@budweiserapac.com>
2024-01-30 16:15:24 +0800

    泛型接口定义及props的使用

---
+++ backup/024-接口泛型自定义类型/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app">
+    <Person/>
+  </div>
+</template>
+<script>
+import Person from "@/components/Person.vue";
+</script>
+<style>
+.app {
+  background-color: aliceblue;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
---
+++ backup/024-接口泛型自定义类型/components/Person.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="person">
+    ???
+  </div>
+
+</template>
+
+<script lang="ts" setup name="Person">
+import {PersonInter, Persons} from "@/types/intex";
+
+let person: PersonInter = {id: 'id01', name: "张三", age: 60}
+
+let personList: Persons = [
+  {id: 'id01', name: "张三", age: 60},
+  {id: 'id02', name: "李四", age: 18},
+  {id: 'id03', name: "王五", age: 66}
+]
+
+</script>
+
+<style scoped>
+.person {
+  background-color: #2c3e50;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+
+button {
+  margin: 5px;
+}
+</style>
